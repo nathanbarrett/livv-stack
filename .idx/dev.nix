6c08cc944455ada04c4e3a3e5324d1651e452fdc
nathan.barrett <nathan.barrett@gmail.com>
2024-05-15 17:06:51 +0000

    add dev.nix file

---
+++ .idx/dev.nix
@@ -0,0 +1,32 @@
+{pkgs}: {
+  channel = "stable-23.11";
+  packages = [
+    pkgs.nodejs_20
+    pkgs.php83Packages.composer
+    pkgs.php83
+  ];
+  idx.extensions = [
+    "vue.volar"
+  ];
+  idx.previews = {
+    previews = {
+      web = {
+        command = [
+          "npm"
+          "run"
+          "dev"
+        ];
+        manager = "web";
+      };
+    };
+  };
+  idx.workspace.onCreate = {
+    create-env = "cp .env.example .env";
+    gen-app-key = "php artisan key:generate";
+  };
+  idx.workspace.onStart = {
+    install-php-deps = "composer install";
+    install-npm-deps = "npm install";
+    ide-helpers = "composer ide-helpers";
+  };
+}
